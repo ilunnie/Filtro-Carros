cd888229ae7fee31dd3972736118b4b4fcb9b810
Luis Gustavo <67930760+ilunnie@users.noreply.github.com>
2023-03-22 20:20:00 -0300

    Comentando
    adicionado comentários sobre o funcionamento das funções e variaveis

--- carros.py
+++ carros.py
@@ -13,6 +13,11 @@ import random
 import re
 #======================
 
+# Retorna apenas quando o usuário digitar um número inteiro
+'''
+    texto = mensagem que irá mostrar ao pedir que o usuário digite
+    erro = mensagem que irá mostrar caso o valor digitado não seja um inteiro
+'''
 def recebe_int(texto, erro = "Valor inválido\n"):
     while True:
         try:
@@ -22,6 +27,11 @@ def recebe_int(texto, erro = "Valor inválido\n"):
         except:
             print(erro)
 
+# Retorna "SIM" ou "NÃO", apenas quando o usuário confirmar sua resposta
+'''
+    texto = mensagem que irá mostrar ao pedir que o usuário digite
+    erro = mensagem que irá mostrar caso o valor digitado não seja uma confirmação válida
+'''
 def recebe_confirmacao(texto, erro="Resposta inválida!\n"):
     while True:
         val = input(texto)
@@ -29,6 +39,12 @@ def recebe_confirmacao(texto, erro="Resposta inválida!\n"):
         if val[0].upper() == 'N': return 'NÃO'
         print(erro)
 
+# Retorna apenas quando o usuário digitar uma placa de carro
+'''
+    texto = mensagem que irá mostrar ao pedir que o usuário digite
+    erro = mensagem que irá mostrar caso o valor digitado não seja uma placa válida ou já tenha sido registrada
+    regex = requisitos para o valor digitado ser considerado válido
+'''
 def recebe_placa(texto, erro = "Placa inválida ou já registrada\n", regex = r"[a-zA-Z]{3}\d{4}"):
     while True:
         placa = input(texto).upper()
@@ -38,6 +54,11 @@ def recebe_placa(texto, erro = "Placa inválida ou já registrada\n", regex = r"
                 return placa
         print(erro)
         
+# Retorna apenas quando o usuário digitar um número de chassi válido
+'''
+    texto = mensagem que irá mostrar ao pedir que o usuário digite
+    erro = mensagem que irá mostrar caso o valor digitado não seja um número válido ou já tenha sido registrado
+'''
 def recebe_chassi(texto, erro = "Chassi inválido ou já registrada\n"):
     while True:
         chassi = input(texto).upper()
@@ -52,13 +73,26 @@ def recebe_chassi(texto, erro = "Chassi inválido ou já registrada\n"):
                 except:
                     None
         print(erro)
-    
+
+# Retorna apenas quando o usuário digitar um tipo de combustível válido
+'''
+    texto = mensagem que irá mostrar ao pedir que o usuário digite
+    erro = mensagem que irá mostrar caso o valor digitado não seja um combustível da lista
+    tipos = lista com os tipos válidos (leva em consideração apenas o primeiro caracter em maiusculo)
+'''
 def recebe_combustivel(texto, erro = 'Combustível inválido\n', tipos = ['G', 'D', 'A']):
     while True:
         comb = input(texto)
         if comb[0].upper() in tipos: return comb[0].upper()
         print(erro)
-    
+
+# Retorna apenas quando o usuário digitar o ano do carro
+'''
+    texto = mensagem que irá mostrar ao pedir que o usuário digite
+    anomax = define o maior ano permitido
+    anomin = define o menor ano permitido
+    erro = mensagem que irá mostrar caso o valor digitado esteja fora do limite aceito (anomin-anomax)
+'''
 def recebe_ano(texto, anomax = 2023, anomin = 1886, erro = 'Ano fora do limite!\n'):
     while True:
         ano = recebe_int(texto)
@@ -68,6 +102,10 @@ def recebe_ano(texto, anomax = 2023, anomin = 1886, erro = 'Ano fora do limite!\
         else:
             print(erro)
     
+# Gera uma nova placa aleatória (não repete as placas)
+'''
+    regex = formato em que a placa deve seguir
+'''
 def gerar_placa(regex = r"[a-zA-Z]{3}\d{4}"):
     chars = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
     while True:
@@ -79,6 +117,7 @@ def gerar_placa(regex = r"[a-zA-Z]{3}\d{4}"):
                 placas_registradas.append(placa_auto)
                 return placa_auto
 
+# Gera um novo número de chassi aleatório (não repete os números de chassi)       
 def gerar_chassi():
     chars = list("0123456789")
     while True:
@@ -89,7 +128,13 @@ def gerar_chassi():
             chassis_regristrados.append(chassi_auto)
             return chassi_auto
 
+# Registra os carros
+'''
+    quant = quantidade de carros a serem registrados
+    forma = forma em que serão registrados ("AUTO" = automatico | "MANUAL" = manual)
+'''
 def regCarro(quant = 0, forma = "AUTO"):
+    # Registra os carros de forma manual
     if forma == "MANUAL":
         try:
             while True:
@@ -104,12 +149,18 @@ def regCarro(quant = 0, forma = "AUTO"):
                 carro["COMBUSTIVEL"] = recebe_combustivel("   Combustível: ")
                 global carros
                 carros.append(carro)
+        # Se apertar crtl+C finaliza o registro
         except KeyboardInterrupt:
             print("\n\n\nFinalizando o registro...")
+    # Registra os carros de forma automática
     elif forma == "AUTO":
+        # Carrega um data frame com nomes brasileiros
         nomes = pandas.read_csv("nomes.csv", sep=',')
+        # Carrega um data frame com modelos de carros
         nomes_carros = pandas.read_csv("marcas-carros.csv", sep=';')
+        # Lista de cores de carros
         cores_lista = ["AZUL", "VERDE", "AMARELO", "ROXO", "ROSA", "VERMELHO", "LARANJA", "MARROM", "CINZA", "BRANCO", "PRETO"]
+        # Lista de combustíveis
         tipos_combustiveis = ['G', 'D', 'A']
         
         for i in range(quant):
@@ -122,17 +173,21 @@ def regCarro(quant = 0, forma = "AUTO"):
             carro["NUM CHASSI"] = gerar_chassi()
             carro["COMBUSTIVEL"] = random.choice(tipos_combustiveis)
             carros.append(carro)
-            
+   
+# Função que troca o proprietario de um carro
 def troca_de_carro(nome):
     chassi = input("Número do chassi: ")
     carro_novo = df[df["NUM CHASSI"] == chassi]
+    # Se encontrar um unico carro com o chassi informado, irá efetuar a troca com sucesso
     if len(carro_novo) == 1:
         i = df.index.get_loc(carro_novo.index[0])
         carros[i]["PROPRIETARIO"] = nome
         return ("Proprietário alterado com sucesso!")
+    # Se não encontrar um unico carro com o chassi informado, irá repassar a mensagem para o usuário
     else:
         return ("Carro não encontrado...")
-    
+
+# Mostra informações sobre um carro com base no número de chassi informado    
 def recebe_carro_chassi(chassi):
     filtrado = df[df["NUM CHASSI"] == chassi]
     if len(filtrado) == 1:
@@ -140,11 +195,16 @@ def recebe_carro_chassi(chassi):
     else:
         return ("Carro não encontrado")
         
-
+# 'carros' é a lista de carros registrados e 'resp' é usada para armazenar as respostas do usuário quando necessário
 carros, resp = [], ''
+# Variaveis que iram guardar as placas e chassis já registrados
 placas_registradas, chassis_regristrados = [], []
 
+
 while True:
+    '''
+        ◖LOOP PRINCÍPAL◗
+    '''
     try:
         resp = recebe_confirmacao("Deseja definir os carros manualmente?\n> ")
         if (resp == 'SIM'): regCarro(forma = "MANUAL")
@@ -178,6 +238,7 @@ while True:
                 resp = ''
             except KeyboardInterrupt:
                         raise KeyboardInterrupt
+    # Finaliza o programa
     except KeyboardInterrupt:
         print("\n\n\nFinalizando o programa...")
         break
